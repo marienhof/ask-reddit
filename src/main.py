b1c3ce1e7446a115495745318cee60738ac82d64
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-03-04 21:50:32 -0800

    Clean up

--- src/main.py
+++ src/main.py
@@ -14,29 +14,30 @@ PROJECT_ID = "reddit-1239"
 
 TABLE_NAME = "%s:reddit_subset.posts_with_responses_sc_cnt_norm" % PROJECT_ID
 
+FIELD_NAMES = ["question", "answer", "count", "score"]
+
 MAX_RESULTS_PER_QUESTION = 30
 
 
-def sql_contains(field_name, s):
-    return ' ((%s CONTAINS "%s ") OR (%s CONTAINS " %s"))' % (field_name, s, field_name, s)
+def sql_contains_word(field_name, word):
+    return ' ((%s CONTAINS "%s ") OR (%s CONTAINS " %s"))' % (field_name, word, field_name, word)
 
 
-def generate_sql(table_name, question_words, only_questions):
-    sql = 'SELECT * FROM [%s] WHERE ' % table_name
-    sql += ' AND '.join([sql_contains('post_title_lower', word) for word in question_words])
-    sql += ' AND response_body_lower != "[deleted]" '
-    if only_questions:
-        sql += ' AND RIGHT(RTRIM(response_body_lower), 1) = "?"'
-    sql += ' ORDER BY cnt LIMIT 10000;'
+def generate_sql(table_name, question_words, only_question_answers):
+    sql = ' '.join([
+        'SELECT * FROM [%s] WHERE' % table_name,
+        ' AND '.join([sql_contains_word('post_title_lower', word) for word in question_words]),
+        'AND response_body_lower != "[deleted]"',
+        'AND RIGHT(RTRIM(response_body_lower), 1) = "?"' if only_question_answers else '',
+        'ORDER BY cnt LIMIT 10000;'
+    ])
     return sql
 
 
-def execute_query(bigquery_service, question, only_questions=False):
-    words = language.filter_stopwords(language.sentence_to_words(question))    
+def execute_query(bigquery, sql):
     try:
-        query_request = bigquery_service.jobs()
-        query_sql = generate_sql(TABLE_NAME, words, only_questions)
-        query_data = { 'query': query_sql }
+        query_data = { 'query': sql }
+        query_request = bigquery.jobs()
         query_response = query_request.query(
             projectId=PROJECT_ID,
             body=query_data).execute()
@@ -44,56 +45,63 @@ def execute_query(bigquery_service, question, only_questions=False):
         print('Error: {}'.format(err.content))
         raise err
     else:
-        results = []
-        field_names = ["question", "answer", "count", "score"]
-        if 'rows' not in query_response:
-            return []
-        for row in query_response['rows']:
-            result = {
-                "is_nasty": False
-            }
-            for (field_name, field) in zip(field_names, row['f']):
-                value = language.clean(field['v'])
-                result["is_nasty"] = language.is_nasty(value) or result["is_nasty"]
-                result[field_name] = value
-            result["question_similarity"] = sentence2vec.similarity(question, result["question"])
-            if (not result["is_nasty"]) and result["question_similarity"] > -.4:
-                results.append(result)
-        return results
+        return query_response    
 
 
-def sort_score(x):
+def retrieve_answers(bigquery, question, only_question_answers=False):
+    words = language.filter_stopwords(language.sentence_to_words(question))
+    sql = generate_sql(TABLE_NAME, words, only_question_answers)
+    query_response = execute_query(bigquery, sql)
+    results = []    
+    if 'rows' not in query_response:
+        return []
+    for row in query_response['rows']:
+        result = {
+            "is_nasty": False
+        }
+        for (field_name, field) in zip(FIELD_NAMES, row['f']):
+            value = language.clean(field['v'])
+            result["is_nasty"] = language.is_nasty(value) or result["is_nasty"]
+            result[field_name] = value
+        result["question_match"] = sentence2vec.similarity(question, result["question"])
+        if (not result["is_nasty"]) and result["question_match"] > -.4:
+            results.append(result)
+    return results
+
+
+def sort_score(result):
     # TODO: adjust parameters
-    return -(x["question_similarity"] + max(float(x["score"]) / 500.0, .2))
+    return -(result["question_match"] + max(float(result["score"]) / 500.0, .2))
 
 
 def print_results(question, results):
     for result in sorted(results, key=sort_score)[:MAX_RESULTS_PER_QUESTION]:
+        answer = colored(result['answer'], 'yellow')
         question = result['question']
-        answer = result['answer']
-        question_similarity = result['question_similarity']
-        score = result['score']
-        print ("=> " + colored(answer, 'yellow') +
-               " (" + question + ") " +
-               colored("{0:.2f}".format(round(question_similarity, 2)), 'green') + ' ' +
-               colored(score, 'red'))
+        question_match = colored("{0:.2f}".format(round(result['question_match'], 2)), 'green')
+        score = colored(result['score'], 'red')
+        print "".join(["=> ", answer, " (", question, ") ", question_match, ' ', score])
 
+
+def interactive_loop(bigquery, only_question_answers):
+    while True:
+        try:
+            question = raw_input('?> ')
+            results = retrieve_answers(bigquery, question, only_question_answers)
+        except KeyboardInterrupt:
+            break
+        else:
+            print_results(question, results)
+            print
+    
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--questions", help="only show follow-up questions", action="store_true")
     args = parser.parse_args()    
     credentials = GoogleCredentials.get_application_default()
-    bigquery_service = build('bigquery', 'v2', credentials=credentials)
-    while True:
-        try:
-            question = raw_input('?> ')
-            results = execute_query(bigquery_service, question, only_questions=args.questions)
-        except KeyboardInterrupt:
-            break
-        else:
-            print_results(question, results[:100])
-            print
+    bigquery = build('bigquery', 'v2', credentials=credentials)
+    interactive_loop(bigquery, only_question_answers=args.questions)
 
 
 if __name__ == '__main__':
